b599486decdada8dcc87b93fe29893770514bbb5
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-04-11 18:27:36 +0000

    feat(server): created express server and mongodb connection

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json());
+
+// Mount routers
+app.use('/api/config', require('./routes/configRoutes'));
+app.use('/api/earnings', require('./routes/earningRoutes'));
+
+// Basic route
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    // server.close(() => process.exit(1)); // Consider if you need this harsh exit
+});

b834b6940b57a1c6671f2b934065fca8fc4eb598
dusanmitrovicoffice <dusanmitrovicoffice@gmail.com>
2025-04-13 18:50:38 +0000

    feat(earnings-model): Changed `guild_id` to String for Discord Snowflake ID compatibility

--- models/Earnings.js
+++ models/Earnings.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-
 const EarningsSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true }, // Assuming this is a unique identifier you generate
-  guild_id: { type: Number, required: true, index: true },
+  guild_id: { type: String, required: true, index: true }, // Changed to String for Discord Snowflake ID compatibility
   date: { type: String, required: true }, // Consider using Date type if format is consistent
   total_cut: { type: Number, required: true },
   gross_revenue: { type: Number, required: true },
@@ -13,5 +12,4 @@ const EarningsSchema = new mongoose.Schema({
   hours_worked: { type: Number, required: true },
   user_mention: { type: String, required: true } // Or potentially a User ID reference
 }, { collection: 'earnings'});
-
 module.exports = mongoose.model('Earnings', EarningsSchema);
